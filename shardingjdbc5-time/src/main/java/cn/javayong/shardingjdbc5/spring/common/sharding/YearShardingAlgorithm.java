7a3a5cb565ae5975845663c8d5f8e54b07969523
LiuZhongkui <1365693530@qq.com>
2026-07-07 14:10:49 +0400

    单表根据时间分表

---
+++ shardingjdbc5-time/src/main/java/cn/javayong/shardingjdbc5/spring/common/sharding/YearShardingAlgorithm.java
@@ -0,0 +1,132 @@
+package cn.javayong.shardingjdbc5.spring.common.sharding;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.sharding.api.sharding.standard.PreciseShardingValue;
+import org.apache.shardingsphere.sharding.api.sharding.standard.RangeShardingValue;
+import org.apache.shardingsphere.sharding.api.sharding.standard.StandardShardingAlgorithm;
+
+import java.util.*;
+
+/**
+ * 按年份分表
+ * <p>
+ * 逻辑表：
+ * t_ent_order_item
+ * <p>
+ * 实际表：
+ * t_ent_order_item_2024
+ * t_ent_order_item_2025
+ * t_ent_order_item_2026
+ * <p>
+ * 分片字段：
+ * create_time
+ */
+@Slf4j
+public class YearShardingAlgorithm implements StandardShardingAlgorithm<Date> {
+
+
+    @Override
+    public void init(Properties props) {
+        log.info("YearShardingAlgorithm init...");
+    }
+
+    /**
+     * =
+     * >
+     * <
+     * in()
+     * 最终都会走这里（单值）
+     */
+    @Override
+    public String doSharding(Collection<String> availableTargetNames,
+                             PreciseShardingValue<Date> shardingValue) {
+
+        Date date = shardingValue.getValue();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int year = calendar.get(Calendar.YEAR);
+
+        String targetTable = shardingValue.getLogicTableName() + "_" + year;
+
+        log.info("逻辑表：{}", shardingValue.getLogicTableName());
+        log.info("年份：{}", year);
+        log.info("目标表：{}", targetTable);
+
+        for (String table : availableTargetNames) {
+            if (table.equalsIgnoreCase(targetTable)) {
+                return table;
+            }
+        }
+
+        throw new UnsupportedOperationException(
+                "不存在目标表：" + targetTable);
+    }
+
+    /**
+     * between
+     * >=
+     * <=
+     */
+    @Override
+    public Collection<String> doSharding(Collection<String> availableTargetNames,
+                                         RangeShardingValue<Date> shardingValue) {
+
+        Collection<String> result = new LinkedHashSet<>();
+
+        Date lower = null;
+        Date upper = null;
+
+        if (shardingValue.getValueRange().hasLowerBound()) {
+            lower = shardingValue.getValueRange().lowerEndpoint();
+        }
+
+        if (shardingValue.getValueRange().hasUpperBound()) {
+            upper = shardingValue.getValueRange().upperEndpoint();
+        }
+
+        if (lower == null && upper == null) {
+            return availableTargetNames;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+
+        int startYear;
+        int endYear;
+
+        if (lower != null) {
+            calendar.setTime(lower);
+            startYear = calendar.get(Calendar.YEAR);
+        } else {
+            startYear = Integer.MIN_VALUE;
+        }
+
+        if (upper != null) {
+            calendar.setTime(upper);
+            endYear = calendar.get(Calendar.YEAR);
+        } else {
+            endYear = Integer.MAX_VALUE;
+        }
+
+        for (String table : availableTargetNames) {
+
+            String suffix = table.substring(table.lastIndexOf("_") + 1);
+
+            int year = Integer.parseInt(suffix);
+
+            if (year >= startYear && year <= endYear) {
+                result.add(table);
+            }
+        }
+
+        log.info("范围查询路由：{}", result);
+
+        return result;
+    }
+
+    @Override
+    public String getType() {
+        return "CLASS_BASED";
+    }
+}
---
+++ shardingjdbc5-time/src/main/java/cn/javayong/shardingjdbc5/spring/service/OrderService.java
@@ -0,0 +1,187 @@
+package cn.javayong.shardingjdbc5.spring.service;
+
+import cn.javayong.shardingjdbc5.spring.domain.mapper.OrderMapper;
+import cn.javayong.shardingjdbc5.spring.domain.po.TEntOrder;
+import cn.javayong.shardingjdbc5.spring.domain.po.TEntOrderDetail;
+import cn.javayong.shardingjdbc5.spring.domain.po.TEntOrderItem;
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private RedisIdGeneratorService redisIdGeneratorService;
+
+    public TEntOrder getOrderById(Long id) {
+        return orderMapper.getOrderById(id);
+    }
+
+    public Map<String, Object> queryOrder(Long orderId) {
+        return orderMapper.queryOrder(orderId);
+    }
+
+    public List<Map<String, Object>> queryOrderOne(Long orderId) {
+        return orderMapper.queryOrderOne(orderId);
+    }
+
+    public List<Map<String, Object>> queryOrderList(Integer page) {
+        return orderMapper.queryOrderList(page);
+    }
+
+    public Integer queryOrderCount() {
+        return orderMapper.queryOrderCount();
+    }
+
+    @Transactional
+    public Long save() {
+        Long entId = 11215L;
+        String regionCode = "BJ";
+
+        //保存订单基本信息
+        TEntOrder tEntOrder = new TEntOrder();
+        Long orderId = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+        tEntOrder.setId(orderId);
+        tEntOrder.setRegionCode(regionCode);
+        tEntOrder.setAmount(new BigDecimal(12.0));
+        tEntOrder.setMobile("150****9235");
+        tEntOrder.setEntId(entId);
+        orderMapper.saveOrder(tEntOrder);
+
+        //保存订单详情
+        TEntOrderDetail tEntOrderDetail = new TEntOrderDetail();
+        Long detailId = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+        tEntOrderDetail.setId(detailId);
+        tEntOrderDetail.setAddress("湖北武汉东西湖区");
+        tEntOrderDetail.setOrderId(orderId);
+        tEntOrderDetail.setEntId(entId);
+        tEntOrderDetail.setStatus(1);
+        tEntOrderDetail.setRegionCode(regionCode);
+        orderMapper.saveOrderDetail(tEntOrderDetail);
+
+        //保存订单条目表
+        {
+            //保存条目 1
+            TEntOrderItem item1 = new TEntOrderItem();
+            Date randomTime = getRandomTime();
+            Long itemId = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+            item1.setId(itemId);
+            item1.setEntId(entId);
+            item1.setOrderId(orderId);
+            item1.setRegionCode("BG");
+            item1.setGoodId("aaaaaaaaaaaa");
+            item1.setGoodName("我的商品111111");
+            item1.setCreateTime(randomTime);
+            orderMapper.saveOrderItem(item1);
+            //保存条目 2
+            TEntOrderItem item2 = new TEntOrderItem();
+            Long itemId2 = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+            item2.setId(itemId2);
+            item2.setEntId(entId);
+            item2.setRegionCode("BJ");
+            item2.setOrderId(orderId);
+            item2.setGoodId("bbbbbbbbbbbb");
+            item2.setGoodName("我的商品22222");
+            item2.setCreateTime(randomTime);
+            orderMapper.saveOrderItem(item2);
+        }
+        return orderId;
+    }
+
+    public void batchsave() {
+        for (int i = 0; i < 100; i++) {
+            Long entId = RandomUtils.nextLong();
+            String regionCode = "BJ";
+
+            //保存订单基本信息
+            TEntOrder tEntOrder = new TEntOrder();
+            Long orderId = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+            tEntOrder.setId(orderId);
+            tEntOrder.setRegionCode(regionCode);
+            tEntOrder.setAmount(new BigDecimal(12.0));
+            tEntOrder.setMobile("150****9235");
+            tEntOrder.setEntId(entId);
+            orderMapper.saveOrder(tEntOrder);
+
+            //保存订单详情
+            TEntOrderDetail tEntOrderDetail = new TEntOrderDetail();
+            Long detailId = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+            tEntOrderDetail.setId(detailId);
+            tEntOrderDetail.setAddress("湖北武汉东西湖区");
+            tEntOrderDetail.setOrderId(orderId);
+            tEntOrderDetail.setEntId(entId);
+            tEntOrderDetail.setStatus(1);
+            tEntOrderDetail.setRegionCode(regionCode);
+            orderMapper.saveOrderDetail(tEntOrderDetail);
+
+            //保存订单条目表
+            {
+                //保存条目 1
+                TEntOrderItem item1 = new TEntOrderItem();
+                Date randomTime = getRandomTime();
+                Long itemId = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+                item1.setId(itemId);
+                item1.setEntId(entId);
+                item1.setOrderId(orderId);
+                item1.setRegionCode("BG");
+                item1.setGoodId("aaaaaaaaaaaa");
+                item1.setGoodName("我的商品111111");
+                item1.setCreateTime(randomTime);
+                orderMapper.saveOrderItem(item1);
+                //保存条目 2
+                TEntOrderItem item2 = new TEntOrderItem();
+                Long itemId2 = redisIdGeneratorService.createUniqueId(String.valueOf(entId));
+                item2.setId(itemId2);
+                item2.setEntId(entId);
+                item2.setRegionCode("BJ");
+                item2.setOrderId(orderId);
+                item2.setGoodId("bbbbbbbbbbbb");
+                item2.setGoodName("我的商品22222");
+                item2.setCreateTime(randomTime);
+                orderMapper.saveOrderItem(item2);
+            }
+        }
+    }
+
+    public static Date getRandomTime() {
+        List<String> yearList = Arrays.asList(
+                "2024-01-01 00:00:00",
+                "2025-01-01 00:00:00",
+                "2026-01-01 00:00:00",
+                "2027-01-01 00:00:00",
+                "2028-01-01 00:00:00",
+                "2029-01-01 00:00:00",
+                "2030-01-01 00:00:00"
+        );
+
+        try {
+            String time = yearList.get(new Random().nextInt(yearList.size()));
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(time);
+        } catch (Exception e) {
+            throw new RuntimeException("随机时间解析失败", e);
+        }
+    }
+
+    public List queryOrderListDemo() {
+        return orderMapper.queryOrderListDemo();
+    }
+
+    // 查询城市列表
+    public List queryCityList() {
+        return orderMapper.queryCityList();
+    }
+
+    public Integer updateCity() {
+        return orderMapper.updateCity();
+    }
+
+}
